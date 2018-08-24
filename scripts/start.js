696d58c883d8dfdd50b8cdfd98a6cab52f481966
taras42 <taras.my.mail@gmail.com>
2018-08-25 01:36:42 +0300

    hit restore delay, collisions in progress

--- scripts/start.js
+++ scripts/start.js
@@ -1,21 +1,43 @@
 (function(GAME) {
-	var spriteSize = 48;
+	var sprite48x48 = 48;
 
-	GAME.loadAssets([
-		{
-			name: "blueFighterIdle",
-			width: spriteSize,
-			height: spriteSize,
+	function getIdleSprite(name) {
+		return {
+			name: name,
+			width: sprite48x48,
+			height: sprite48x48,
 			frameCount: 4,
 			fps: 2
-		},
-		{
-			name: "blueFighterWalk",
-			width: spriteSize,
-			height: spriteSize,
+		}
+	}
+
+	function getWalkSprite(name) {
+		return {
+			name: name,
+			width: sprite48x48,
+			height: sprite48x48,
 			frameCount: 2,
 			fps: 4
-		}
+		};
+	}
+
+	function getHitSprite(name) {
+		return {
+			name: name,
+			width: 64,
+			height: 48,
+			frameCount: 1,
+			fps: 1
+		};
+	}
+
+	GAME.loadAssets([
+		getIdleSprite("blueFighterIdle"),
+		getWalkSprite("blueFighterWalk"),
+		getHitSprite("blueFighterHit"),
+		getIdleSprite("redFighterIdle"),
+		getWalkSprite("redFighterWalk"),
+		getHitSprite("redFighterHit")
 	], function() {
 		GAME.createObject({
 			objectName: "blueFighter",
@@ -24,10 +46,34 @@
 			x: 0,
 			y: 0
 		}, function(fighter) {
+			fighter.defaultRestoreHitDelay = GAME.settings.FPS;
+			fighter.restoreHitDelay = fighter.defaultRestoreHitDelay;
+			fighter.damage = 10;
+			fighter.balance = 100;
 			fighter.speed = 1;
 			fighter.keys = {
 				forward: [68],
-				back: [65]
+				back: [65],
+				hit: [83]
+			};
+		});
+
+		GAME.createObject({
+			objectName: "redFighter",
+			defaultAsset: GAME.assets.redFighterIdle,
+			loop: true,
+			x: 100,
+			y: 0
+		}, function(fighter) {
+			fighter.defaultRestoreHitDelay = GAME.settings.FPS;
+			fighter.restoreHitDelay = fighter.defaultRestoreHitDelay;
+			fighter.damage = 10;
+			fighter.balance = 100;
+			fighter.speed = 1;
+			fighter.keys = {
+				forward: [74],
+				back: [76],
+				hit: [75]
 			};
 		});
 
--- scripts/update.js
+++ scripts/update.js
@@ -1,30 +1,77 @@
 (function(GAME) {
 
-	function doesFighterWalk(object, direction) {
-		return object.keys[direction].find(function(key) {
-			return GAME.input.keys[key];
-		});
+	function isKeyPressedPredicate(key) {
+		return GAME.input.keys[key];
 	}
 
-	function updateFighterPosition(fighter, context, delta) {
+	function doesFighterWalkInDirection(fighter, direction) {
+		return fighter.keys[direction].find(isKeyPressedPredicate);
+	}
+
+	function doesFighterHit(fighter) {
+		return !fighter.cooldown 
+			&& !fighter.stunned 
+			&& fighter.keys.hit.find(isKeyPressedPredicate);
+	}
+
+	function doesFighterWalk(fighter) {
+		return {
+			forward: doesFighterWalkInDirection(fighter, "forward"),
+			back: doesFighterWalkInDirection(fighter, "back")
+		}
+	}
+
+	function getHitDamage(fighter1, fighter2, inverseDirection) {
+		var fighter1Asset = fighter1.currentAsset.asset,
+			fighter2Asset = fighter2.currentAsset.asset,
+			damage;
+
+		if (inverseDirection) {
+			if (fighter2.x - fighter2Asset.width < fighter1.x + fighter1Asset.width/2) {
+				return fighter2.damage;
+			}
+		} else {
+			if (fighter1.x + fighter1Asset.width > fighter2.x + fighter2Asset.width/2) {
+				return fighter1.damage;
+			}
+		}
+	}
+
+	function setFightersStateAfterHit(fighter1, fighter2, damage) {
+		fighter1.cooldown = true;
+		fighter2.stunned = true;
+		fighter2.balance -= damage;
+	}
+
+	function tryRestoreFighterHit(fighter, asset, hitAsset) {
+		var hasHitAsset = fighter.currentAsset.asset === hitAsset;
+
+		if (fighter.cooldown || fighter.stunned || hasHitAsset) {
+			fighter.restoreHitDelay -= 1;
+
+			if (fighter.restoreHitDelay === 0) {
+				fighter.restoreHitDelay = fighter.defaultRestoreHitDelay;
+				fighter.cooldown = false;
+				fighter.stunned = false;
+
+				fighter.setAsset(asset, true);
+			}
+		}
+	}
+
+	function getFighterNextPosition(fighter, walkState, context, delta) {
 		var canvasWidth = context.canvas.clientWidth,
 			scaleX = GAME.settings.scale.x;
 
-		var forward = doesFighterWalk(fighter, "forward"),
-			back = doesFighterWalk(fighter, "back"),
+		var forward = walkState.forward,
+			back = walkState.back,
 			walks = forward || back,
 			currentAsset = fighter.currentAsset.asset,
-			walkAsset = GAME.assets.blueFighterWalk,
-			idleAsset = GAME.assets.blueFighterIdle,
 			speed,
 			xBoundary,
-			x;
+			x = fighter.x;
 
 		if (walks) {
-			if (currentAsset !== walkAsset) {
-				fighter.setAsset(walkAsset, true);
-			}
-
 			speed = fighter.speed * delta;
 
 			if (forward) {
@@ -40,14 +87,58 @@
 			}
 
 			fighter.x = x;
-		} else if (currentAsset !== idleAsset) {
-			fighter.setAsset(idleAsset, true);
 		}
+
+		return x;
+	};
+
+	function trySetFighterPosition() {
+		
+	}
+
+	function updateFightersState(blueFighter, redFighter, context, delta) {
+		var blueFighterHits = doesFighterHit(blueFighter),
+			redFighterHits = doesFighterHit(redFighter),
+			damage;
+
+		var blueFighterWalkState = doesFighterWalk(blueFighter),
+			redFighterWalkState = doesFighterWalk(redFighter);
+
+		var blueFighterNextPosition = getFighterNextPosition(blueFighter, blueFighterWalkState, context, delta),
+			redFighterNextPosition = getFighterNextPosition(redFighter, redFighterWalkState, context, delta);
+
+		if (blueFighterHits) {
+			damage = getHitDamage(blueFighter, redFighter);
+
+			blueFighter.setAsset(GAME.assets.blueFighterHit, true);
+
+			if (damage)  {
+				setFightersStateAfterHit(blueFighter, redFighter, damage);
+			}
+		} else {
+			trySetFighterPosition();
+		}
+
+		if (redFighterHits) {
+			damage = getHitDamage(redFighter, blueFighter, true);
+
+			redFighter.setAsset(GAME.assets.redFighterHit, true);
+
+			if (damage)  {
+				setFightersStateAfterHit(redFighter, blueFighter, damage);
+			}
+		} else {
+			trySetFighterPosition();
+		}
+
+		tryRestoreFighterHit(blueFighter, GAME.assets.blueFighterIdle, GAME.assets.blueFighterHit);
+		tryRestoreFighterHit(redFighter, GAME.assets.redFighterIdle, GAME.assets.redFighterHit);
 	}
 
 	GAME.updateObjects = function(context, delta) {
-		var blueFighter = GAME.objects.blueFighter;
+		var blueFighter = GAME.objects.blueFighter,
+			redFighter = GAME.objects.redFighter;
 
-		updateFighterPosition(blueFighter, context, delta);
+		updateFightersState(blueFighter, redFighter, context, delta);
 	};
 })(GAME);
